6fec50273fab652880e73a55097cfb40c5969808
Krzysztof Hermaszewski <hermaszewski.krzysztof@gmail.com>
2022-01-18 17:54:46 +0100

    Add project files.

---
+++ WebApp/Program.cs
@@ -0,0 +1,36 @@
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using MyApp.ApplicationLogic;
+using MyApp.Repository;
+using MyApp.Repository.ApiClient;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApp
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("#app");
+
+            builder.Services.AddSingleton<IWebApiExecuter>(sp => new WebApiExecuter("https://localhost:44314", new HttpClient()));
+
+            builder.Services.AddTransient<IProjectsScreenUseCases, ProjectsScreenUseCases>();
+            builder.Services.AddTransient<IProjectRepository, ProjectRepository>();
+            builder.Services.AddTransient<ITicketRepository, TicketRepository>();
+            builder.Services.AddTransient<ITicketsScreenUseCases, TicketsScreenUseCases>();
+            builder.Services.AddTransient<ITicketScreenUseCases, TicketScreenUseCases>();
+
+            //builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+            await builder.Build().RunAsync();
+        }
+    }
+}
